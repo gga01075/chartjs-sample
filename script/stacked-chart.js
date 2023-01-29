f141b7e9876362bce5975b98f9eeeb7b1d37831d
gga01075 <gga01075@naver.com>
2023-01-29 19:59:43 +0900

    stacked bar chart

--- script/stacked-chart.js
+++ script/stacked-chart.js
@@ -4,40 +4,79 @@ let myChartThree = document.getElementById('myChartThree');
 let barChartThree = new Chart(myChartThree, {
   type: "bar",
   data : {
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    labels: ['비교기간', '분석기간'],
     datasets: [{
-      label: 'Weekly Sales',
-      data: [18, 12, 6, 9, 12, 3, 9],
+      label: '정상',
+      data: [4.3, 2.5],
       backgroundColor: [
         'rgba(0, 0, 0, 0.2)'
       ],
       borderColor: [
         'rgba(0, 0, 0, 1)'
       ],
-      borderWidth: 1
+      borderWidth: 1,
+      datalabels: {
+        align: 'center',
+        anchor: 'center'
+      }
     },
     {
-        label: 'Weekly Sales',
-        data: [18, 12, 6, 9, 12, 3, 9],
+        label: '취소',
+        data: [2.4, 4.4],
         backgroundColor: [
           'rgba(255, 26, 104, 0.2)',
         ],
         borderColor: [
           'rgba(255, 26, 104, 1)',
         ],
-        borderWidth: 1
-      },{
-        label: 'Line Sales',
-        data: [18, 12, 6, 9, 12, 3, 9],
+        borderWidth: 1,
+        datalabels: {
+            align: 'center',
+            anchor: 'center'
+          }
+      },
+    {
+        label: '반품/교환',
+        data: [2, 2],
+        backgroundColor: [
+          'rgba(255, 255, 104, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 255, 104, 0.2)',
+        ],
+        borderWidth: 1,
+        datalabels: {
+            align: 'center',
+            anchor: 'center'
+          }
+      },
+      
+      
+      
+      
+      
+      
+      
+      
+      {
+        label: '정상',
+        data: [4.3, 2.5],
         backgroundColor: 'rgba(0, 0, 0, 0.2)',
         borderColor:'rgba(0, 0, 0, 1)',
         tension: 0,
         type : 'line'
       },{
-        label: 'Line Sales',
-        data: [18, 12, 6, 9, 12, 3, 9],
+        label: '취소',
+        data: [2.4, 4.4],
         backgroundColor: 'rgba(255, 26, 104, 0.2)',
         borderColor:'rgba(255, 26, 104, 1)',
+        tension: 0,
+        type : 'line'
+      },{
+        label: '반품/교환',
+        data: [2, 2],
+        backgroundColor:  '#4444ff',
+        borderColor: '#4444ff',
         tension: 0,
         type : 'line'
       }]
@@ -52,6 +91,7 @@ let barChartThree = new Chart(myChartThree, {
         stacked : true,
       }
     }
-  }
+  },
+  
 
 });
